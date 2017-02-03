fa107c5541c6a6604c3b8a34e232eb043b01eea6
robertweber95 <robertweber95@gmail.com>
2017-02-02 20:19:35 -0500

    Add redux-thunk middleware

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import appReducer from './reducers/appReducer'
 import AddTodo from './components/addTodo'
 import VisibleTodoList from './components/todoListConsole'
@@ -15,7 +16,7 @@ const App = () => (
   </div>
 )
 
-const store = createStore(appReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(appReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk));
 store.subscribe(() => { console.log(store.getState()) })
 
 render(
